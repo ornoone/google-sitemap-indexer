0b579885dafebaea5e2a15148c2121ceecbba962
ornoone <darius@xornot.fr>
2024-09-28 11:47:41 +0200

    runserver and first template

--- app/google_indexer/urls.py
+++ app/google_indexer/urls.py
@@ -16,8 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+
+from django.conf import settings
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name = "base.html"), name='index'),
     path('admin/', admin.site.urls),
 ]
 
+
+
+if settings.DEBUG:
+
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
