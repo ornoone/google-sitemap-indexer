43b1d71fe2aa205a336879122ffdc734f04937d6
ornoone <darius@xornot.fr>
2024-09-29 16:37:12 +0200

    enhance stuff

--- app/google_indexer/apps/indexer/views/tracked_site.py
+++ app/google_indexer/apps/indexer/views/tracked_site.py
@@ -9,7 +9,7 @@ from django.views.generic.edit import FormMixin, ProcessFormView
 
 from google_indexer.apps.indexer.form import TrackedSiteForm
 from google_indexer.apps.indexer.models import TrackedSite, TrackedPage, PAGE_STATUS_NEED_INDEXATION, \
-    SITE_STATUS_PENDING, PAGE_STATUS_CREATED, PAGE_STATUS_PENDING_VERIFICATION
+    SITE_STATUS_PENDING, PAGE_STATUS_CREATED, PAGE_STATUS_PENDING_VERIFICATION, PAGE_STATUS_PENDING_INDEXATION_CALL
 from google_indexer.apps.indexer.tasks import update_sitemap, verify_page, index_page
 
 
@@ -38,8 +38,10 @@ class TrackedSiteDetailView(DetailView):
     model = TrackedSite
     def get_context_data(self, **kwargs):
         top10 = []
+        status_filter = self.request.GET.get('status')
         for status, _ in TrackedPage._meta.get_field("status").flatchoices:
-            top10.extend(self.object.pages.filter(status=status).order_by('id')[:10])
+            if status_filter is None or status_filter == status:
+                top10.extend(self.object.pages.filter(status=status).order_by('id')[:10])
         return super().get_context_data(top10=top10, **kwargs)
 
 
@@ -71,6 +73,9 @@ class TrackedSiteActionView(SingleObjectMixin, View):
 
             object.pages.update(status=PAGE_STATUS_CREATED)
             messages.success(self.request, "all %d pages reseted" % object.pages.count())
+        elif action == 'reset_pending':
+            object.pages.filter(status=PAGE_STATUS_PENDING_VERIFICATION).update(status=PAGE_STATUS_CREATED)
+            object.pages.filter(status=PAGE_STATUS_PENDING_INDEXATION_CALL).update(status=PAGE_STATUS_NEED_INDEXATION)
         else:
             messages.error(self.request, "unknown action %s" % action)
         return HttpResponseRedirect(reverse('indexer:site-detail', kwargs={'pk': object.id}))
