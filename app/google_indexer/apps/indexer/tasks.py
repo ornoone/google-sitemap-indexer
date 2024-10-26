55764d9c028ae368cd4bca800fc1aff055f30e85
MaximeDuvinage <88149050+MaximeDuvinage@users.noreply.github.com>
2024-10-26 09:30:05 +0200

    Update tasks.py

--- app/google_indexer/apps/indexer/tasks.py
+++ app/google_indexer/apps/indexer/tasks.py
@@ -41,7 +41,7 @@ def chunks(lst, n):
 
 @db_periodic_task(crontab(minute="*"))  # On execute toute les minutes au lieu de 5 */5.
 def index_pages():
-    max_pending = 150 # on essaye une valeur à la main (60 / WAIT_BETWEEN_VALIDATION_SECONDS) * 5.
+    max_pending = 60 # on essaye une valeur à la main (60 / WAIT_BETWEEN_VALIDATION_SECONDS) * 5.
     chunk_size = max(0, max_pending - TrackedPage.objects.filter(status=PAGE_STATUS_PENDING_INDEXATION_CALL).count())
     now = timezone.now()
     if not has_available_apikey(now):
@@ -205,4 +205,3 @@ def index_page(page_id):
     print(f"Nombre de tâches restantes dans la file d'attente : {remaining_tasks}")
     if retry:
         raise CancelExecution()
-
