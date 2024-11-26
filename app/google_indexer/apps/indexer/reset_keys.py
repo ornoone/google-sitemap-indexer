dbb6fc91bc7ee067c702fcd02f2f8cd94fa2dd63
MaximeDuvinage <88149050+MaximeDuvinage@users.noreply.github.com>
2024-11-26 09:29:50 +0100

    Delete app/google_indexer/apps/indexer/reset_keys.py

--- app/google_indexer/apps/indexer/reset_keys.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from google_indexer.apps.indexer.tasks import reset_keys_function
-
-class Command(BaseCommand):
-    help = 'Reset the key usages manually'
-
-    def handle(self, *args, **kwargs):
-        reset_keys_function()
-        self.stdout.write(self.style.SUCCESS('Successfully reset the key usages'))
